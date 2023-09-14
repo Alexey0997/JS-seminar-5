b20f663779acd950553028900f8747475db390c2
alexey <asivlev@yandex.ru>
2023-09-14 23:42:59 +0300

    Main is the entry point to the program

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -1,5 +1,16 @@
-package ru.geekbrains;public class Main {
+package ru.geekbrains;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\nРЕЗЕРВНОЕ КОПИРОВАНИЕ ФАЙЛОВ\n");
+        BackUpper backUpper = new BackUpper();
+        backUpper.filesBackUpper(Path.of("."));
+
+        System.out.println("\nПЕЧАТЬ ДЕРЕВА ДИРЕКТОРИЙ И ФАЙЛОВ\n");
+        Tree.print(new File("."), "", true); // Вызываем метод вывода на печать.
     }
 }
